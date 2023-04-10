79fd6c75b100de88f83ce991ced994fe4369052f
RWallace1ro <wal1ro@aol.com>
2023-04-09 20:58:08 -0400

    New update for my project

--- scripts/index.js
+++ scripts/index.js
@@ -24,7 +24,6 @@ const initialCards = [
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg",
   },
 ];
-console.log(initialCards);
 
 /*---------------------------------------------------------------------------------------------------------*/
 /*                                                Element                                                  */
@@ -42,13 +41,28 @@ const profileDescriptionInput = document.querySelector(
 );
 
 const profileEditForm = profileEditModal.querySelector(".modal__form");
-
+const cardListEl = document.querySelector(".cards__list");
+const cardTemplate =
+  document.querySelector("#card-template").content.firstElementChild;
 /*---------------------------------------------------------------------------------------------------------*/
 /*                                                Functions                                                */
 /*---------------------------------------------------------------------------------------------------------*/
 function closePopup() {
   profileEditCloseModal.classList.remove("modal__opened");
 }
+
+function getCardElement(cardData) {
+  const cardElement = cardTemplate.cloneNode(true);
+  const cardImageEl = cardElement.querySelector(".card__image");
+  const cardTitleEl = cardElement.querySelector(".card__title");
+
+  cardImageEl.src = cardData.link;
+  cardImageEl.alt = cardData.name;
+  cardTitleEl.textContent = cardData.name;
+
+  return cardElement;
+}
+
 /*---------------------------------------------------------------------------------------------------------*/
 /*                                                Event Handlers                                           */
 /*---------------------------------------------------------------------------------------------------------*/
@@ -70,3 +84,8 @@ profileEditButton.addEventListener("click", () => {
 profileEditCloseButton.addEventListener("click", closePopup);
 
 profileEditForm.addEventListener("submit", handleProfileEditSubmit);
+
+initialCards.forEach((cardData) => {
+  const cardElement = getCardElement(cardData);
+  cardListEl.prepend(cardElement);
+});
